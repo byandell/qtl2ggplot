5e3907466c47ea149bc4e9b85b418fdbfa7c83b4
Karl Broman <kbroman@gmail.com>
2017-03-06 13:18:26 -0600

    Revise plot_snpasso; new argument snpinfo
     - Revised due to changes in qtl2scan

--- R/plot_snpasso.R
+++ R/plot_snpasso.R
@@ -7,9 +7,29 @@
 #' \code{\link[qtl2scan]{scan1}} are run with SNP probabilities
 #' produced by \code{\link[qtl2scan]{genoprob_to_snpprob}}.
 #'
+#' @param snpinfo Data frame with SNP information with the following
+#'     columns (the last three are generally derived from with
+#'     \code{\link[qtl2scan]{index_snps}}):
+#' \itemize{
+#' \item \code{chr} - Character string or factor with chromosome
+#' \item \code{pos} - Position (in same units as in the \code{"map"}
+#'     attribute in \code{genoprobs}.
+#' \item \code{sdp} - Strain distribution pattern: an integer, between
+#'     1 and \eqn{2^n - 2} where \eqn{n} is the number of strains, whose
+#'     binary encoding indicates the founder genotypes
+#' \item \code{snp} - Character string with SNP identifier (if
+#'     missing, the rownames are used).
+#' \item \code{index} - Indices that indicate equivalent
+#'     groups of SNPs.
+#' \item \code{intervals} - Indexes that indicate which marker
+#'     intervals the SNPs reside.
+#' \item \code{on_map} - Indicate whether SNP coincides with a marker
+#'     in the \code{genoprobs}
+#' }
+#'
 #' @param lodcolumn LOD score column to plot (a numeric index, or a
 #' character string for a column name). One or more value(s) allowed.
-#' 
+#'
 #' @param show_all_snps If TRUE, expand to show all SNPs.
 #'
 #' @param drop.hilit SNPs with LOD score within this amount of the maximum SNP association will be highlighted.
@@ -60,6 +80,9 @@
 #' # switch map in allele probabilities to Mbp
 #' apr$map <- DOex$pmap
 #'
+#' # identify equivalent snps
+#' snpinfo <- index_snps(DOex$pmap, snpinfo)
+#'
 #' # convert to snp probabilities
 #' snppr <- genoprob_to_snpprob(apr, snpinfo)
 #'
@@ -68,77 +91,94 @@
 #'
 #' # plot results
 #' library(qtl2ggplot)
-#' plot_snpasso(out_snps)
+#' plot_snpasso(out_snps, snpinfo)
 #'
 #' # can also just type plot()
-#' plot(out_snps)
+#' plot(out_snps, snpinfo)
 #'
 #' # plot just subset of distinct SNPs
-#' plot_snpasso(out_snps, show_all_snps=FALSE)
+#' plot_snpasso(out_snps, snpinfo, show_all_snps=FALSE)
 #'
 #' # highlight the top snps (with LOD within 1.5 of max)
-#' plot(out_snps, drop.hilit=1.5)
-#' 
+#' plot(out_snps, snpinfo, drop.hilit=1.5)
+#'
 #' # highlight SDP patterns in SNPs; connect with lines.
-#' plot(out_snps, patterns="all",drop.hilit=4)
-#' 
+#' plot(out_snps, snpinfo, patterns="all",drop.hilit=4)
+#'
 #' # highlight top SDP patterns in SNPs; connect with lines.
-#' plot(out_snps, patterns="hilit",drop.hilit=4)
+#' plot(out_snps, snpinfo, patterns="hilit",drop.hilit=4)
 #' }
 #'
 #' @seealso \code{\link{plot_scan1}}, \code{\link{plot_coef}}, \code{\link{plot_coefCC}}
-#' 
+#'
 #' @export
 #' @importFrom CCSanger sdp_to_pattern
-#' 
+#'
 plot_snpasso <-
-    function(scan1output, lodcolumn=1, show_all_snps=TRUE, drop.hilit=NA,
+    function(scan1output, snpinfo, lodcolumn=1, show_all_snps=TRUE, drop.hilit=NA,
              col.hilit="violetred", col="darkslateblue",
              ylim=NULL, gap=25,
              bgcolor="gray90", altbgcolor="gray85",
              ...)
 {
-    plot_snpasso_internal(scan1output, lodcolumn, show_all_snps, drop.hilit,
+    uindex <- unique(snpinfo$index)
+    if(length(uindex) != nrow(scan1output))
+        stop("Something is wrong with snpinfo$index.\n",
+             "      length(unique(snpinfo$index)) [",
+             length(unique(snpinfo$index)), "] != nrow(scan1output) [",
+             nrow(scan1output), "].")
+
+    if(any(snpinfo$index[uindex] != uindex))
+        stop("Something is wrong with snpinfo$index.\n",
+             "      snpinfo$index[u] should == u for values in snpinfo$index")
+
+    plot_snpasso_internal(scan1output, snpinfo, lodcolumn, show_all_snps, drop.hilit,
                           col.hilit, col,
                           ylim, gap,
                           bgcolor, altbgcolor,
                           ...)
 }
 
-plot_snpasso_internal <- function(scan1output, lodcolumn, show_all_snps, drop.hilit,
+plot_snpasso_internal <- function(scan1output, snpinfo, lodcolumn, show_all_snps, drop.hilit,
                                   col.hilit, col,
                                   ylim, gap,
                                   bgcolor, altbgcolor,
                                   patterns = c("none","all","hilit"),
                                   lines = (patterns != "none"), points = TRUE,
-                                  legend.position = ifelse((patterns != "none"), "right", "none"), 
+                                  legend.position = ifelse((patterns != "none"), "right", "none"),
                                   legend.title = ifelse((patterns != "none"), "pattern", "pheno"),
                                   ...) {
+
+  map <- snpinfo_to_map(snpinfo)
+
   patterns <- match.arg(patterns)
   if(patterns != "none")
-    pattern <- CCSanger::sdp_to_pattern(scan1output$snpinfo[[1]]$sdp)
-  
-  if(show_all_snps)
-    scan1output <- expand_snp_results(scan1output)
-  
+    pattern <- CCSanger::sdp_to_pattern(snpinfo$sdp)
+
+  if(show_all_snps) {
+      tmp <- expand_snp_results(scan1output, map, snpinfo)
+      scan1output <- tmp$lod
+      map <- tmp$map
+  }
+
   # maximum LOD
-  maxlod <- max(scan1output$lod[,lodcolumn], na.rm=TRUE)
-  
+  maxlod <- max(unclass(scan1output)[,lodcolumn], na.rm=TRUE)
+
   if(is.null(ylim))
-    ylim <- c(max(0, min(scan1output$lod[,lodcolumn], na.rm=TRUE)),
+    ylim <- c(max(0, min(unclass(scan1output)[,lodcolumn], na.rm=TRUE)),
               maxlod*1.02)
-  
-  settings <- color_patterns_set(scan1output, patterns,
-                                  col, pattern, show_all_snps, 
+
+  settings <- color_patterns_set(scan1output, snpinfo, patterns,
+                                  col, pattern, show_all_snps,
                                   col.hilit, drop.hilit, maxlod)
   # settings$pattern will be either SDP patterns or thresholding by drop.hilit.
-  
-  plot_scan1(scan1output, lodcolumn=lodcolumn, bgcolor=bgcolor, altbgcolor=altbgcolor, ylim=ylim,
-             gap = gap, 
+
+  plot_scan1(scan1output, map=map, lodcolumn=lodcolumn, bgcolor=bgcolor, altbgcolor=altbgcolor, ylim=ylim,
+             gap = gap,
              col = settings$col,
              pattern = settings$pattern,
              shape = settings$shape,
-             lines = lines, points = points, 
+             lines = lines, points = points,
              legend.position = legend.position,
              legend.title = legend.title,
              patterns = patterns,
@@ -148,31 +188,67 @@ plot_snpasso_internal <- function(scan1output, lodcolumn, show_all_snps, drop.hi
 
 # expand snp association results according to snpinfo
 expand_snp_results <-
-    function(snp_results)
+    function(snp_results, map, snpinfo)
 {
-    snpinfo <- snp_results$snpinfo
-    if(is.null(snpinfo)) stop("No snpinfo found")
-    map <- snp_results$map
-    if(is.null(map)) stop("No map found")
+    snpinfo <- split(snpinfo, snpinfo$chr)
 
     if(length(map) != length(snpinfo))
         stop("length(map) [", length(map), "] != length(snpinfo) [",
              length(snpinfo), "]")
 
-    if(nrow(snp_results$lod) != length(unlist(map)))
-        stop("nrow(snp_results$lod) [", nrow(snp_results$lod), "] != length(unlist(map)) [",
+    if(nrow(snp_results) != length(unlist(map)))
+        stop("nrow(snp_results) [", nrow(snp_results), "] != length(unlist(map)) [",
              length(unlist(map)), "]")
 
-    lodindex <- split(seq(along=snp_results$lod[,1]), rep(names(map), vapply(map, length, 0)))
+    lodindex <- split(1:nrow(snp_results), rep(names(map), vapply(map, length, 0)))
 
     result <- NULL
     for(i in seq(along=map)) {
+        revindex <- rev_snp_index(snpinfo[[i]])
+
         map[[i]] <- snpinfo[[i]]$pos
         names(map[[i]]) <- snpinfo[[i]]$snp
-        result <- rbind(result, snp_results$lod[lodindex[[i]],,drop=FALSE][snpinfo[[i]]$index,,drop=FALSE])
+        result <- rbind(result, unclass(snp_results)[lodindex[[i]],,drop=FALSE][revindex,,drop=FALSE])
+        rownames(result) <- snpinfo[[i]]$snp
     }
 
-    snp_results$map <- map
-    snp_results$lod <- result
-    snp_results
+    list(lod=result,
+         map=map)
+}
+
+# snpinfo to map
+snpinfo_to_map <-
+    function(snpinfo)
+{
+    uindex <- sort(unique(snpinfo$index))
+    if(any(snpinfo$index < 1 | snpinfo$index > nrow(snpinfo)))
+        stop("snpinfo$index values outside of range [1, ",
+             nrow(snpinfo), "]")
+
+    uchr <- unique(snpinfo$chr)
+    chr <- factor(snpinfo$chr, levels=uchr)
+
+    map <- split(snpinfo$pos, chr)
+    snp <- split(snpinfo$snp, chr)
+    index <- split(snpinfo$index, chr)
+    for(i in seq(along=map)) {
+        u <- unique(index[[i]])
+        map[[i]] <- map[[i]][u]
+        names(map[[i]]) <- snp[[i]][u]
+    }
+
+    names(map) <- uchr
+
+    map
+}
+
+# reverse index
+rev_snp_index <-
+    function(snpinfo)
+{
+    index_spl <- split(1:nrow(snpinfo), snpinfo$index)
+    revindex <- rep(seq(along=index_spl), vapply(index_spl, length, 1))
+    revindex[unlist(index_spl)] <- revindex
+
+    revindex
 }
