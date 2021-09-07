dab3c4f8ec327d0bf1311f112ca835f5c691bf2e
Brian Yandell <byandell@wisc.edu>
2021-09-07 12:24:36 -0500

    1.1 update to CRAN; remove example for local copy of sdp_to_pattern as this is now exported by qtl2pattern

--- R/sdp_to_pattern.R
+++ R/sdp_to_pattern.R
@@ -1,6 +1,7 @@
 #' Convert sdp to pattern
 #'
 #' Convert strain distribution pattern (sdp) to letter pattern.
+#' Taken from package `qtl2pattern` for internal use here.
 #'
 #' @param sdp vector of sdp values
 #' @param haplos letter codes for haplotypes (required)
@@ -9,24 +10,6 @@
 #'
 #' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
 #' @keywords utilities
-#'
-#' @examples
-#' dirpath <- "https://raw.githubusercontent.com/rqtl/qtl2data/master/DOex"
-#' 
-#' # Download SNP info for DOex from web and read as RDS.
-#' tmpfile <- tempfile()
-#' download.file(file.path(dirpath, "c2_snpinfo.rds"), tmpfile, quiet=TRUE)
-#' snpinfo <- readRDS(tmpfile)
-#' unlink(tmpfile)
-#' snpinfo <- dplyr::rename(snpinfo, pos = pos_Mbp)
-#' 
-#' # Extract strain distribution pattern.
-#' sdp <- snpinfo$sdp
-#' # Find out how many alleles.
-#' nallele <- ceiling(log2(max(sdp)))
-#' out <- sdp_to_pattern(sdp, LETTERS[seq_len(nallele)])
-#' # Show most frequent patterns. 
-#' head(rev(sort(c(table(out)))))
 #'
 #' @importFrom assertthat assert_that
 #' 
@@ -37,12 +20,5 @@ sdp_to_pattern <- function(sdp, haplos) {
     paste(paste(haplos[!ref], collapse = ""),
           paste(haplos[ref], collapse = ""),
           sep = ":")
-  }, haplos)
-}
-#' @rdname sdp_to_pattern
-sdp_to_logical <- function(sdp, haplos) {
-  assertthat::assert_that(!missing(haplos))
-  sapply(sdp, function(x, haplos) {
-    as.logical(intToBits(x)[seq_along(haplos)])
   }, haplos)
 }
